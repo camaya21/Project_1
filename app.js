12c28611adbc36fb363710da29110b3362def567
camaya21 <cindymamaya21@gmail.com>
2022-11-16 22:50:01 -0500

    Added timer function and setInterval

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const startBtn = document.querySelector('#startButton');
 const scoreStat = document.querySelector('#score');
+const countDown = document.querySelector('#countDown');
 
 const wrongBtn = document.querySelectorAll('.wrongAnswer');
 wrongBtn.forEach(wrongAnswer => {
@@ -13,20 +14,31 @@ correctBtn.forEach(correctAnswer => {
 
 startBtn.addEventListener('click', startGame)
 function startGame(){
+    interval = setInterval(timer, 1000)
     if (correctBtn){
     }
     if (wrongBtn){
     }
-    // add timer
 }
 
 function wrongMessage(){
     alert('WRONGGGG')
+    wrongBtn.forEach(wrongAnswer => {
+        wrongAnswer.removeEventListener('click', wrongMessage);
+    });
 }
 
 function increaseScore() {
     scoreStat.textContent =  Number(scoreStat.textContent)+1
-    if (scoreStat.textContent== 1) {
+    if (scoreStat.textContent == 1) {
         alert('YOU WIN')
+    }
+}
+
+function timer() {
+    countDown.textContent = Number(countDown.textContent)-1
+    if (countDown.textContent == 0){
+        clearInterval(interval)
+        return alert('You LOSE! You took too long')
     }
 }
