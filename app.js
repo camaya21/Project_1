8be18dbfe99ccc0dc8defc0ef677f848fab0862a
camaya21 <cindymamaya21@gmail.com>
2022-11-17 14:21:38 -0500

    Complete element.removeEventListeners

--- app.js
+++ app.js
@@ -4,41 +4,48 @@ const countDown = document.querySelector('#countDown');
 
 const wrongBtn = document.querySelectorAll('.wrongAnswer');
 wrongBtn.forEach(wrongAnswer => {
-    wrongAnswer.addEventListener('click', wrongMessage);
+    wrongAnswer.addEventListener('click', wrongMessage,);
 });
 
 const correctBtn = document.querySelectorAll('.correctAnswer');
 correctBtn.forEach(correctAnswer => {
-    correctAnswer.addEventListener('click', increaseScore)
-})
+    correctAnswer.addEventListener('click', increaseScore);
+});
 
 startBtn.addEventListener('click', startGame)
 function startGame(){
-    interval = setInterval(timer, 1000)
-    if (correctBtn){
-    }
-    if (wrongBtn){
-    }
+    interval= setInterval(timer, 1000);
+    correctBtn || wrongBtn;
 }
 
 function wrongMessage(){
-    alert('WRONGGGG')
-    wrongBtn.forEach(wrongAnswer => {
-        wrongAnswer.removeEventListener('click', wrongMessage);
-    });
+    alert('WRONGGGG');
+    resetGame()
 }
 
 function increaseScore() {
     scoreStat.textContent =  Number(scoreStat.textContent)+1
     if (scoreStat.textContent == 1) {
-        alert('YOU WIN')
+        alert('YOU WIN');
+        resetGame()
     }
 }
 
 function timer() {
     countDown.textContent = Number(countDown.textContent)-1
     if (countDown.textContent == 0){
-        clearInterval(interval)
-        return alert('You LOSE! You took too long')
+        resetGame();
+        return alert('You LOSE! You took too long');
     }
+}
+
+function resetGame() {
+    startBtn.removeEventListener('click', startGame);
+    clearInterval(interval);
+    wrongBtn.forEach(wrongAnswer => {
+        wrongAnswer.removeEventListener('click', wrongMessage,);
+    });
+    correctBtn.forEach(correctAnswer => {
+        correctAnswer.removeEventListener('click', increaseScore);
+    });
 }
