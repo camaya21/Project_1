24464994da62cdb7ae3f852f9fa2cb73f0c09888
camaya21 <cindymamaya21@gmail.com>
2022-11-18 10:30:38 -0500

    Created an array for questions

--- app.js
+++ app.js
@@ -3,6 +3,20 @@ const scoreStat = document.querySelector('#score');
 const countDown = document.querySelector('#countDown');
 const questionContainer = document.getElementById('question-container')
 
+const q1 = document.getElementById('q1')
+const q2 = document.getElementById('q2')
+const q3 = document.getElementById('.q3')
+const q4 = document.getElementById('.q4')
+// const q5 = document.querySelector('.q5')
+// const q6 = document.querySelector('.q6')
+// const q7 = document.querySelector('.q7')
+// const q8 = document.querySelector('.q8')
+// const q9 = document.querySelector('.q9')
+// const q10 = document.querySelector('.q10') q3, q4, q5, q6, q7, q8, q9, q10
+
+let myArray = [q1, q2, q3, q4];
+let i = 0
+
 const wrongBtn = document.querySelectorAll('.wrongAnswer');
 wrongBtn.forEach(wrongAnswer => {
     wrongAnswer.addEventListener('click', wrongMessage);
@@ -16,13 +30,14 @@ correctBtn.forEach(correctAnswer => {
 startBtn.addEventListener('click', startGame);
 function beginning() {
     startBtn.classList.add('hide');
-    questionContainer.classList.remove('hide')
+    myArray[i].classList.remove('hide')
+    i+=1
 }
 
 function startGame(){
     beginning();
     interval= setInterval(timer, 1000);
-    correctBtn || wrongBtn;
+    correctBtn || wrongBtn
 }
 
 function wrongMessage(){
