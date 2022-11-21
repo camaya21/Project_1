1171a5d93f7d4dc1795ae758c35076ab1f189c4a
camaya21 <cindymamaya21@gmail.com>
2022-11-20 23:35:19 -0500

    Wrong Answer shows on screen

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const basicNum = document.getElementById('basicNum')
 const scoreStat = document.querySelector('#score');
 const countDown = document.querySelector('#countDown');
 const questionContainer = document.getElementById('question-container')
-
+const wrongAlert = document.getElementById('alert')
 const q1 = document.getElementById('q1')
 const q2 = document.getElementById('q2')
 const q3 = document.getElementById('q3')
@@ -27,75 +27,106 @@ const getRanQues = () => {
         random.push(question);
     }
 }
-    getRanQues(source);
-    console.log(random[0].classList);
-    
-    const correctBtn = document.querySelectorAll('.correctAnswer');
-    const wrongBtn = document.querySelectorAll('.wrongAnswer');
-    
+getRanQues(source);
+console.log(random[0].classList);
+
+// const closeBtn = document.querySelector('.close-button')
+const correctBtn = document.querySelectorAll('.correctAnswer');
+const wrongBtn = document.querySelectorAll('.wrongAnswer');
+function addlisteners() {
     correctBtn.forEach(correctAnswer => {
         correctAnswer.addEventListener('click', increaseScore);
     });
     wrongBtn.forEach(wrongAnswer => {
         wrongAnswer.addEventListener('click', wrongMessage);
     });
+}
+
+startBtn.addEventListener('click', startGame);
+function beginning() {
+    addlisteners()
+    startBtn.classList.add('hide');
+    basicNum.classList.remove('hide');
+    random[i].classList.remove('hide');
+}
+
+function startGame(){
+    beginning();
+    interval= setInterval(timer, 1000);
+}
+
+// closeBtn.forEach(wrongAnswer => {
+//     wrongAnswer.addEventListener('click', wrongAlert.classList.add('hide'))
+// })
+function wrongMessage(){
+    // alert('WRONGGGG');
+    removeListener();
+    wrongAlert.classList.remove('hide')
+    myTimeout = setTimeout(hideMessage, 5000)
+    //     setTimeout(hideMessage(), 5000)
+    //     // hideRemove(i);
+    //     // i++
+}
     
-    startBtn.addEventListener('click', startGame);
-    function beginning() {
-        startBtn.classList.add('hide');
-        basicNum.classList.remove('hide');
-        random[i].classList.remove('hide');
+function hideMessage(){
+    wrongAlert.classList.add('hide')
+    addlisteners();
+    hideRemove(i);
+    i++
+}
+
+function increaseScore() {
+    scoreStat.textContent =  Number(scoreStat.textContent)+1
+    if (scoreStat.textContent <=10) {
+        alert('CORRECT')
     }
+    hideRemove(i);
+    i++
+}
+// function hideRemove(i){
+//     random[i].classList.add('hide');
+//     random[i+1].classList.remove('hide');
+//     countDown.textContent = 5
+//     interval= setInterval(timer, 1000);
+//     console.log(i);
+//     if (random[i]===random[9]) return
+// }
     
-    function startGame(){
-        beginning();
-        interval= setInterval(timer, 1000);
+function hideRemove(i) {
+    addlisteners();
+    random[i].classList.add("hide");
+    if (i + 1 < source.length) {
+      random[i + 1].classList.remove("hide");
+      clearInterval(interval);
+      countDown.textContent = 20;
+      interval = setInterval(timer, 1000);
+      console.log(i);
+      
+    }else {
+      console.log('game over')
+      alert('gameOver')
     }
-    
-    function wrongMessage(){
-        alert('WRONGGGG');
+}
+
+console.log(random[9])
+function timer() {
+    countDown.textContent = Number(countDown.textContent)-1
+    if (countDown.textContent == 0){
         clearInterval(interval);
+        alert('You LOSE! You took too long');
         hideRemove(i);
         i++
     }
-    
-    function increaseScore() {
-        scoreStat.textContent =  Number(scoreStat.textContent)+1
-        if (scoreStat.textContent <=10) {
-            alert('GOODJOB');
-        }
-        clearInterval(interval);
-        hideRemove(i);
-        i++
-    }
-    function hideRemove(i){
-        random[i].classList.add('hide');
-        random[i+1].classList.remove('hide');
-        countDown.textContent = 5
-        interval= setInterval(timer, 1000);
-        console.log(i);
-        if (random[i]===random[9]) return
-    }
-    
-    console.log(random[9])
-    function timer() {
-        countDown.textContent = Number(countDown.textContent)-1
-        if (countDown.textContent == 0){
-            clearInterval(interval);
-            alert('You LOSE! You took too long');
-            hideRemove(i);
-            i++
-        }
-    }
+}
     
     //scoreEnd.textContent = scoreStat.textContent
-// function removeListener() {
-//     //startBtn.removeEventListener('click', startGame);
-//     clearInterval(interval);
-//     wrongBtn.forEach(wrongAnswer => {
-//         wrongAnswer.removeEventListener('click', wrongMessage,);
-//     });
-//     correctBtn.forEach(correctAnswer => {
-//         correctAnswer.removeEventListener('click', increaseScore);
-//     });
-// }
+function removeListener() {
+    //startBtn.removeEventListener('click', startGame);
+    clearInterval(interval);
+    wrongBtn.forEach(wrongAnswer => {
+        wrongAnswer.removeEventListener('click', wrongMessage,);
+    });
+    correctBtn.forEach(correctAnswer => {
+        correctAnswer.removeEventListener('click', increaseScore);
+    });
+}
